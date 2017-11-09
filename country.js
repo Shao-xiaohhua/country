8c3c615060c560fc3fd3484ac223ab8b9980e5b8
sxh <shaoxiaohua@homolo.com>
2017-11-09 15:20:22 +0800

    add country

---
+++ country.js
@@ -0,0 +1,312 @@
+var c = {
+  A: {
+    list: [
+      {name: '阿鲁巴(荷)', language: ['荷兰语', '英语']},
+      {name: '阿曼', language: ['阿拉伯语', '英语']},
+      {name: '阿联酋', language: ['阿拉伯语', '英语']},
+      {name: '阿尔巴尼亚', language: ['阿尔巴尼亚语', '英语']},
+      {name: '爱沙尼亚', language: ['爱沙尼亚语', '英语']},
+      {name: '阿塞拜疆', language: ['英语', '俄语', '阿塞拜疆语']},
+      {name: '澳大利亚', language: ['英语']},
+      {name: '埃塞俄比亚', language: ['英语', '阿姆哈拉语']},
+      {name: '安哥拉', language: ['英语', '葡萄牙语']},
+      {name: '埃及', language: ['英语', '法语', '阿拉伯语']},
+      {name: '爱尔兰', language: ['英语', '爱尔兰语']},
+      {name: '阿富汗', language: ['英语', '波斯语']},
+      {name: '安圭拉(英)', language: ['英语']},
+      {name: '阿根廷', language: ['英语', '西班牙语']},
+      {name: '安道尔', language: ['英语', '加泰罗尼亚语']},
+      {name: '安提瓜和巴布达', language: ['英语']},
+      {name: '阿尔及利亚', language: ['英语', '阿拉伯语', '法语']},
+      {name: '奥地利', language: ['英语', '德语']}
+    ]
+  },
+  B: {
+    list: [
+      {name: '布基纳法索', language: ['英语', '法语']},
+      {name: '波斯尼亚', language: ['英语', '斯洛文尼亚语']},
+      {name: '百慕大(英)', language: ['英语']},
+      {name: '不丹', language: ['宗卡语', '英语']},
+      {name: '巴勒斯坦', language: ['阿拉伯语', '英语']},
+      {name: '比利时', language: ['英语', '佛兰芒语', '法语']},
+      {name: '巴西', language: ['英语', '葡萄牙语']},
+      {name: '巴基斯坦', language: ['英语', '乌尔都语']},
+      {name: '巴布亚新几内亚', language: ['英语', '莫土语']},
+      {name: '贝宁', language: ['英语', '法语']},
+      {name: '玻利维亚', language: ['英语', '西班牙语']},
+      {name: '巴巴多斯', language: ['英语']},
+      {name: '北马里亚纳(美)', language: ['英语']},
+      {name: '博茨瓦纳', language: ['英语', '茨瓦纳语']},
+      {name: '巴拉圭', language: ['英语', '瓜拉尼语', '西班牙语']},
+      {name: '保加利亚', language: ['英语', '保加利亚语']},
+      {name: '冰岛', language: ['英语', '冰岛语']},
+      {name: '巴林', language: ['英语', '阿拉伯语']},
+      {name: '巴哈马', language: ['英语']},
+      {name: '白俄罗斯', language: ['英语', '俄语']},
+      {name: '波兰', language: ['英语', '波兰语']},
+      {name: '巴拿马', language: ['英语', '西班牙语']},
+      {name: '伯利兹', language: ['英语', '西班牙语']},
+      {name: '布隆迪', language: ['英语', '法语']},
+      {name: '波多黎各(美)', language: ['英语', '西班牙语']}
+    ]
+  },
+  C: {
+    list: [
+      {name: '赤道几内亚', language: ['英语', '西班牙语']},
+      {name: '朝鲜', language: ['英语', '朝鲜语']}
+    ]
+  },
+  D: {
+    list: [
+      {name: '多米尼克', language: ['英语']},
+      {name: '多哥', language: ['英语', '法语']},
+      {name: '多米尼加共和国', language: ['英语', '西班牙语']},
+      {name: '东帝汶', language: ['葡萄牙语', '英语']},
+      {name: '德国', language: ['德语', '英语']},
+      {name: '丹麦', language: ['英语', '丹麦语']}
+    ]
+  },
+  E: {
+    list: [
+      {name: '厄立特里亚', language: ['英语', '提格雷尼亚语']},
+      {name: '俄罗斯', language: ['英语', '俄语']},
+      {name: '厄瓜多尔', language: ['英语', '西班牙语', '克丘亚语']}
+    ]
+  },
+  F: {
+    list: [
+      {name: '芬兰', language: ['英语', '芬兰语']},
+      {name: '菲律宾', language: ['英语', '菲律宾语']},
+      {name: '法属波利尼西亚', language: ['英语', '塔希提语', '法语']},
+      {name: '斐济群岛', language: ['斐济群岛语', '英语']},
+      {name: '法罗群岛(丹)', language: ['法罗语', '英语']},
+      {name: '法属圭亚那', language: ['英语', '法语']},
+      {name: '佛得角', language: ['英语', '葡萄牙语', '克里奥尔语']},
+      {name: '梵蒂冈', language: ['英语', '意大利语']},
+      {name: '法国', language: ['英语', '法语']}
+    ]
+  },
+  G: {
+    list: [
+      {name: '圭亚那', language: ['英语']},
+      {name: '刚果共和国', language: ['英语', '法语']},
+      {name: '冈比亚', language: ['英语']},
+      {name: '关岛(美)', language: ['夏莫罗语', '英语']},
+      {name: '刚果民主共和国', language: ['法语', '英语']},
+      {name: '瓜德罗普(法)', language: ['英语', '法语', '克里奥尔语']},
+      {name: '古巴', language: ['英语', '西班牙语']},
+      {name: '格林纳达', language: ['英语', '法语']},
+      {name: '格鲁吉亚', language: ['英语', '格鲁吉亚语']},
+      {name: '格陵兰(丹)', language: ['英语', '格陵兰语', '丹麦语']},
+      {name: '哥伦比亚', language: ['英语', '西班牙语']},
+      {name: '哥斯达黎加', language: ['英语', '西班牙语']}
+    ]
+  },
+  H: {
+    list: [
+      {name: '海地', language: ['英语', '法语', '克里奥尔语']},
+      {name: '荷属安的列斯', language: ['英语', '帕彼曼都语', '荷兰语']},
+      {name: '洪都拉斯', language: ['英语', '西班牙语', '印第安语方言']},
+      {name: '荷兰', language: ['弗里斯语', '荷兰语', '英语']},
+      {name: '韩国', language: ['韩语', '英语']},
+      {name: '哈萨克斯坦', language: ['英语', '俄语', '哈萨克语']}
+    ]
+  },
+  J: {
+    list: [
+      {name: '几内亚', language: ['英语', '法语']},
+      {name: '吉尔吉斯斯坦', language: ['英语', '吉尔吉语', '俄语']},
+      {name: '几内亚比绍', language: ['英语', '克里奥尔语', '葡萄牙语']},
+      {name: '捷克', language: ['捷克语', '英语']},
+      {name: '加蓬', language: ['法语', '英语', '巴太凯', '米耶内', '芳语']},
+      {name: '加拿大', language: ['英语', '法语']},
+      {name: '柬埔寨', language: ['英语', '法语', '高棉语']},
+      {name: '津巴布韦', language: ['英语']},
+      {name: '基里巴斯', language: ['英语', '基里巴斯语']},
+      {name: '加纳', language: ['英语']},
+      {name: '加那利群岛', language: ['英语', '西班牙语']},
+      {name: '吉布提', language: ['英语', '阿拉伯语', '法语']}
+    ]
+  },
+  K: {
+    list: [
+      {name: '科特迪瓦', language: ['英语']},
+      {name: '库克群岛(新)', language: ['英语', '毛利语']},
+      {name: '克罗地亚', language: ['英语', '斯洛文尼亚语']},
+      {name: '肯尼亚', language: ['斯瓦希里语', '英语']},
+      {name: '卡塔尔', language: ['法语', '阿拉伯语']},
+      {name: '科摩罗', language: ['英语', '法语']},
+      {name: '喀麦隆', language: ['英语', '法语']},
+      {name: '开曼群岛(英)', language: ['英语']},
+      {name: '基里巴斯', language: ['英语', '基里巴斯语']},
+      {name: '科威特', language: ['英语', '阿拉伯语']},
+      {name: '凯科斯群岛(英)', language: ['英语']}
+    ]
+  },
+  L: {
+    list: [
+      {name: '莱索托', language: ['英语', '塞苏陀语']},
+      {name: '罗马尼亚', language: ['英语', '罗马尼亚语']},
+      {name: '留尼旺(法)', language: ['英语', '地方法语']},
+      {name: '利比里亚', language: ['英语']},
+      {name: '拉脱维亚', language: ['英语', '拉脱维亚语']},
+      {name: '卢旺达', language: ['英语', '法语', '卢旺达语']},
+      {name: '列支敦士登', language: ['英语', '德语']},
+      {name: '黎巴嫩', language: ['英语', '阿拉伯语']},
+      {name: '立陶宛', language: ['英语', '立陶宛语']},
+      {name: '卢森堡', language: ['英语', '意大利语']},
+      {name: '老挝', language: ['英语', '老挝语', '法语']},
+      {name: '利比亚', language: ['英语', '阿拉伯语', '意大利语']}
+    ]
+  },
+  M: {
+    list: [
+      {name: '美国', language: ['英语']},
+      {name: '摩洛哥', language: ['英语', '阿拉伯语']},
+      {name: '摩尔多瓦', language: ['英语', '俄语']},
+      {name: '密克罗尼西亚', language: ['英语']},
+      {name: '马绍尔群岛', language: ['英语']},
+      {name: '马达加斯加', language: ['英语', '法语']},
+      {name: '马其顿', language: ['英语', '克罗地亚语']},
+      {name: '蒙特塞拉特(英)', language: ['英语']},
+      {name: '摩纳哥', language: ['英语', '法语']},
+      {name: '缅甸', language: ['英语', '缅甸语']},
+      {name: '毛里塔尼亚', language: ['英语', '阿拉伯语', '法语']},
+      {name: '秘鲁', language: ['英语', '盖丘亚语', '西班牙语', '阿伊马拉语']},
+      {name: '马德拉群岛(葡)', language: ['英语', '葡萄牙语']},
+      {name: '美属萨摩亚', language: ['英语', '萨摩亚语']},
+      {name: '美属维尔京群岛', language: ['英语']},
+      {name: '墨西哥', language: ['英语', '西班牙语']},
+      {name: '马尔代夫', language: ['英语', '迪维希语']},
+      {name: '孟加拉国', language: ['英语', '孟加拉语']},
+      {name: '莫桑比克', language: ['英语', '葡萄牙语']},
+      {name: '马提尼克(法)', language: ['英语', '法语']},
+      {name: '墨塞哥维那', language: ['英语', '斯洛文尼亚语']},
+      {name: '马里', language: ['英语', '班巴拉语', '法语']},
+      {name: '马耳他', language: ['英语', '马耳他语']},
+      {name: '马来西亚', language: ['英语', '马来语']},
+      {name: '马拉维', language: ['英语', '奇契瓦语']},
+      {name: '毛里求斯', language: ['英语', '印地语']}
+    ]
+  },
+  N: {
+    list: [
+      {name: '南非', language: ['英语', '南非荷兰语']},
+      {name: '纳米比亚', language: ['英语', '南非荷兰语']},
+      {name: '尼日利亚', language: ['英语', '豪萨语']},
+      {name: '挪威', language: ['英语', '挪威语']},
+      {name: '尼日尔', language: ['英语', '法语', '豪萨语']},
+      {name: '尼泊尔', language: ['英语', '尼泊尔语']},
+      {name: '纽埃(新)', language: ['英语', '纽埃语']},
+      {name: '尼加拉瓜', language: ['英语', '西班牙语']}
+    ]
+  },
+  P: {
+    list: [
+      {name: '帕劳', language: ['英语']},
+      {name: '葡萄牙', language: ['英语', '葡萄牙语']},
+      {name: '皮特凯恩岛(英)', language: ['英语']}
+    ]
+  },
+  R: {
+    list: [
+      {name: '日本', language: ['英语', '中文', '日语']},
+      {name: '瑞士', language: ['英语', '法语', '意大利语', '罗曼语', '德语']},
+      {name: '瑞典', language: ['英语', '瑞典语']}
+    ]
+  },
+  S: {
+    list: [
+      {name: '索马里', language: ['英语', '索马里语']},
+      {name: '圣卢西亚', language: ['英语', '克里奥尔语']},
+      {name: '塞内加尔', language: ['英语', '法语']},
+      {name: '萨尔瓦多', language: ['英语', '西班牙语']},
+      {name: '斯洛文尼亚', language: ['英语', '斯洛文尼亚语']},
+      {name: '圣马力诺', language: ['英语', '意大利语']},
+      {name: '圣文森特和格林纳丁斯', language: ['英语']},
+      {name: '圣多美及普林西比', language: ['英语', '葡萄牙语']},
+      {name: '斯威士兰', language: ['英语', '斯瓦蒂语']},
+      {name: '圣赫勒拿(英)', language: ['英语']},
+      {name: '圣基茨和尼维斯', language: ['英语']},
+      {name: '苏丹', language: ['英语', '阿拉伯语']},
+      {name: '苏里南', language: ['英语', '苏里南语', '荷兰语']},
+      {name: '萨摩亚', language: ['英语', '萨摩亚语']},
+      {name: '斯洛伐克', language: ['英语', '捷克语']},
+      {name: '塞舌尔', language: ['英语', '法语']},
+      {name: '斯里兰卡', language: ['英语', '僧伽罗语']},
+      {name: '塞浦路斯', language: ['英语', '希腊语', '土耳其语']},
+      {name: '沙特阿拉伯', language: ['英语', '阿拉伯语']},
+      {name: '塞拉利昂', language: ['英语']},
+      {name: '塞尔维亚', language: ['英语', '克罗地亚语']},
+      {name: '所罗门群岛', language: ['英语', '皮金语']}
+    ]
+  },
+  T: {
+    list: [
+      {name: '土库曼斯坦', language: ['英语', '俄语']},
+      {name: '突尼斯', language: ['英语', '法语', '阿拉伯语']},
+      {name: '塔吉克斯坦', language: ['英语', '塔吉克语', '俄语']},
+      {name: '托克劳(新)', language: ['英语', '托克劳语']},
+      {name: '泰国', language: ['英语', '泰语']},
+      {name: '土耳其', language: ['英语', '土耳其语']},
+      {name: '坦桑尼亚', language: ['英语', '班图语']},
+      {name: '特克斯和凯科斯群岛(英)', language: ['英语']},
+      {name: '图瓦卢', language: ['英语', '图瓦卢语']},
+      {name: '汤加', language: ['英语', '汤加语']}
+    ]
+  },
+  W: {
+    list: [
+      {name: '乌克兰', language: ['英语', '俄语']},
+      {name: '委内瑞拉', language: ['英语', '印第安语', '西班牙语']},
+      {name: '瓦努阿图', language: ['英语', '法语']},
+      {name: '乌拉圭', language: ['英语', '西班牙语']},
+      {name: '危地马拉', language: ['英语', '印第安语', '西班牙语']},
+      {name: '乌兹别克斯坦', language: ['英语', '乌兹别克语', '俄语']},
+      {name: '乌干达', language: ['英语', '斯瓦希里语']},
+      {name: '瓦利斯与富图纳(法)', language: ['英语', '波利尼西亚语', '法语']},
+      {name: '文莱', language: ['英语', '马来语', '中文']},
+      {name: '汤加', language: ['英语', '汤加语']}
+    ]
+  },
+  X: {
+    list: [
+      {name: '匈牙利', language: ['英语', '匈牙利语']},
+      {name: '叙利亚', language: ['英语', '阿拉伯语']},
+      {name: '新西兰', language: ['英语', '毛利语']},
+      {name: '希腊', language: ['英语', '希腊语']},
+      {name: '新喀里多尼亚(法)', language: ['英语', '法语']},
+      {name: '新加坡', language: ['英语', '中文']},
+      {name: '西撒哈拉', language: ['英语', '阿拉伯语']},
+      {name: '西班牙', language: ['英语', '西班牙语']}
+    ]
+  },
+  Y: {
+    list: [
+      {name: '越南', language: ['英语', '越南语']},
+      {name: '约旦', language: ['英语', '阿拉伯语']},
+      {name: '伊朗', language: ['英语', '波斯语']},
+      {name: '亚速尔群岛(葡)', language: ['英语', '葡萄牙语']},
+      {name: '也门', language: ['英语', '阿拉伯语']},
+      {name: '英国', language: ['英语']},
+      {name: '印度', language: ['英语', '印地语']},
+      {name: '英属维尔京群岛', language: ['英语']},
+      {name: '亚美尼亚', language: ['英语', '亚美尼亚语', '俄语']},
+      {name: '意大利', language: ['英语', '意大利语']},
+      {name: '印度尼西亚', language: ['英语', '荷兰语', '印尼语']},
+      {name: '以色列', language: ['英语', '阿拉伯语', '希伯莱语']},
+      {name: '牙买加', language: ['英语', '克里奥尔语']},
+      {name: '伊拉克', language: ['英语', '阿拉伯语']}
+    ]
+  },
+  Z: {
+    list: [
+      {name: '赞比亚', language: ['英语', '奔巴语']},
+      {name: '乍得', language: ['英语', '法语']},
+      {name: '中非', language: ['英语', '桑戈语', '法语']},
+      {name: '瑙鲁', language: ['英语', '瑙鲁语']},
+      {name: '智利', language: ['英语', '马普切语', '西班牙语']}
+    ]
+  }
+}
+exports.Country = c
